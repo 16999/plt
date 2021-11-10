90acc53ee358ed0e231acf0c18e85c4bce45b021
16999 <faisonslepoint@protonmail.com>
2021-11-10 16:09:30 +0100

    affichage map

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,14 +1,48 @@
 #include <render/Scene.h>
 
+render::Scene map;
 
-render::Scene s(960,540,"TANKS GAME");
 
 int main(int argc,char* argv[])
 {
-  //refresh();
-  s.init();
-  while(1)
-  {
-    s.refresh();
-  }
+
+  sf::RenderWindow window(sf::VideoMode(512, 256), "Tilemap");
+
+      // define the level with an array of tile indices
+      const int level[] =
+      {
+          0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+          0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+          1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+          0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+          0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+          0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+          2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+          0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+      };
+
+
+      // create the tilemap from the level definition
+
+      //Tilemap map;
+      map.load("../res/tileset.png", sf::Vector2u(32, 32), level, 16, 8);
+      // run the main loop
+      while (window.isOpen())
+      {
+          // handle events
+          sf::Event event;
+          while (window.pollEvent(event))
+          {
+              if(event.type == sf::Event::Closed)
+                  window.close();
+          }
+
+          // draw the map
+          window.clear();
+          window.draw(map);
+          window.display();
+      }
+
+      return 0;
+
 }
--- src/client/render/Scene.cpp
+++ src/client/render/Scene.cpp
@@ -5,21 +5,65 @@ using namespace render;
 using namespace state;
 
 
-Scene::Scene(int width, int height, std::string title)
+/*Scene::Scene(int width, int height, std::string title)
 {
   this->width = width;
   this->height = height;
   this->title = title;
-}
+}*/
 
 void Scene::init()
 {
   std::cout << "-- Welcome to " << this->title << endl;
-  window.create(sf::VideoMode(this->width,this->height), this->title);
+  //window.create(sf::VideoMode(this->width,this->height), this->title);
 }
 
 void Scene::refresh(void)
 {
-  window.clear(sf::Color::Blue);
-  window.display();
+  //window.clear(sf::Color::Blue);
+  //window.display();
+}
+
+void Scene::load(std::string tileset, sf::Vector2u tileSize, const int* tiles, int width, int height)
+{
+
+  m_tileset.loadFromFile(tileset);        // load the tileset texture
+  m_vertices.setPrimitiveType(sf::Quads); // resize the vertex array to fit the level size
+  m_vertices.resize(width * height * 4);
+
+  // populate the vertex array, with one quad per tile
+  for (int i = 0; i < width; i++)
+      for (int j = 0; j < height; j++)
+      {
+          // get the current tile number
+          int tileNumber = tiles[i + j * width];
+
+          // find its position in the tileset texture
+          int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+          int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+          // get a pointer to the current tile's quad
+          sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+
+          // define its 4 corners
+          quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+          quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+          quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+          quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+
+          // define its 4 texture coordinates
+          quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+          quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+          quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+          quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+      }
+
+}
+
+
+void Scene::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+  states.transform *= getTransform();
+  states.texture = &m_tileset;          // apply the tileset texture
+  target.draw(m_vertices, states);      // draw the vertex array
 }
