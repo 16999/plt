5793650f304e0eb185a18495b276de1431790aee
10265 <LINshuman>
2021-11-22 16:59:44 +0100

    Bullet.cpp

--- src/shared/state/Bullet.cpp
+++ src/shared/state/Bullet.cpp
@@ -1,28 +1,38 @@
-//#include "weapon.h"
+#include "Bullet.h"
 #include <math.h>
 #include <cmath>
-//using namespace element;
+using namespace state;
+using namespace std;
 
-float Element::Bullet() {
+
+float Bullet::positionInit(){
+    return x,y;
+}
+
+float Bullet::positionFin(){
+    return x,y;
+}
+
+
+void Bullet::positionBullet() {
     float Vinit = 10;
     float RADinit = M_PI/4;
     float t;
-    Element::positionInit;
-    Element::positionFin;
     for(t=0;t<2;t+=0.1)
     {
     positionFin = CalculPosition (Vinit, RADinit, t, positionInit);
-    std::cout << "La position finale à " << t << "s soit ("<< positionFin.x << "," << positionFin.y << ")" << std::endl;
+    std::cout << "La position finale à " << t << "s soit ("<< positionFin.x << "," << positionFin.y << ")" << endl;
     }
-    return positionFin;
+    return 0;
 }
 
-Element::positionFin CalculPosition (float Vinit, float RADinit, float t, Position positionInit)
+Bullet::positionFin CalculPosition (float Vinit, float RADinit, float t, Element::positionInit)
 {
     //vitesse en m.s-1, position en m, angle en radians et temps en secondes
     //opération efectuée dans le plan 2D xy
 
-    float g = 9.8;   //m.s-2
+
+float g = 9.8;   //m.s-2
     Position positionFin;
 
     positionFin.x = Vinit * cos(RADinit) * t + positionInit.x;
