fd0ab939f5884cdb3144ad2506f3e61e8f98b4d5
10265 <shumanLIN>
2021-11-23 10:00:10 +0100

    Element

--- src/shared/state/Bullet.cpp
+++ src/shared/state/Bullet.cpp
@@ -3,39 +3,47 @@
 #include <cmath>
 using namespace state;
 using namespace std;
+/*
+BUllet::Bullet(){
 
-
-float Bullet::positionInit(){
-    return x,y;
 }
 
-float Bullet::positionFin(){
-    return x,y;
+Bullet::~Bullet(){
+
 }
+ */
 
-
-void Bullet::positionBullet() {
-    float Vinit = 10;
-    float RADinit = M_PI/4;
-    float t;
-    for(t=0;t<2;t+=0.1)
-    {
-    positionFin = CalculPosition (Vinit, RADinit, t, positionInit);
-    std::cout << "La position finale à " << t << "s soit ("<< positionFin.x << "," << positionFin.y << ")" << endl;
-    }
-    return 0;
-}
-
-Bullet::positionFin CalculPosition (float Vinit, float RADinit, float t, Element::positionInit)
+sf::float positionBullet (float t)
 {
     //vitesse en m.s-1, position en m, angle en radians et temps en secondes
     //opération efectuée dans le plan 2D xy
+    //float Vinit, float RADinit,
+    //positionFin = CalculPosition (Vinit, RADinit, t, positionInit);
+    float g = 9.8;   //m.s-2
+    float x,y;
+    x = Element::getX() const;
+
+    for(t=0;t<2;t+=0.1)
+    {
+        Element::positionFin.x = Vinit * cos(RADinit) * t + positionInit.x;
+        Element::positionFin.y = Vinit * sin(RADinit) * t - 0.5 * g * pow(t,2);
+        std::cout << "La position finale à " << t << "s soit ("<< positionFin.x << "," << positionFin.y << ")" << endl;
+    }
 
 
-float g = 9.8;   //m.s-2
-    Position positionFin;
-
-    positionFin.x = Vinit * cos(RADinit) * t + positionInit.x;
-    positionFin.y = Vinit * sin(RADinit) * t - 0.5 * g * pow(t,2);
     return positionFin;
 }
+
+/*
+ * float float Bullet::getPositionInit(){
+    return x,y;
+}
+*/
+float Bullet::getVandRAD() {
+    float Vinit = 10;
+    float RADinit = M_PI/4;
+    float t;
+
+    return Vinit, RADinit;
+}
+
