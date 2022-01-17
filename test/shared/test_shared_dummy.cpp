01e95adf8d5acb16da3688bdb2a107001eb29a26
Davson1 <91320347+Davson1@users.noreply.github.com>
2022-01-17 10:25:02 +0000

    Udapte tests
    ajout de test

--- test/shared/test_shared_dummy.cpp
+++ test/shared/test_shared_dummy.cpp
@@ -8,6 +8,7 @@
 #include "../../src/shared/engine/Engine.h"
 #include "../../src/shared/engine/KeyboardCommand.h"
 
+#include "../../src/shared/ai.h"
 #include "../../src/shared/ai/DumbAI.h"
 #include "../../src/shared/ai/HeuristicAI.h"
 #include "../../src/shared/ai/AdvancedAI.h"
@@ -127,6 +128,27 @@ BOOST_AUTO_TEST_CASE(TestRecord)
     BOOST_CHECK_EQUAL(record.getActionNumber(),23);
     record.setGameNumber(8);
     BOOST_CHECK_EQUAL(record.getGameNumber(),8);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(TestDumbAI)
+{
+  {
+    AI* AI1=new DumbAI(0);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(TestHeuristicAI)
+{
+  {
+    AI* AI2=new HeuristicAI(0);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(TestAdvancedAI)
+{
+  {
+    AI* AI3=new AdvancedAI(0);
   }
 }
 
